d044abe1b57cb687f33c95597bf0d5b833d8929b
LuizRedGalaxy <luizgustavoglt@gmail.com>
2024-10-25 15:21:03 -0300

    Alterado ex1 lab2

--- Lab2/Ex1/main.cpp
+++ Lab2/Ex1/main.cpp
@@ -5,102 +5,101 @@
  * Autores: Hendrick Felipe Scheifer, João Victor Briganti, Luiz Takeda
  * Licença: BSD 2
  *
- * Data: 21/10/2024
+ * Data: 25/10/2024
  */
-#include <cstdio>     // printf()
 #include <cstdlib>    // atoi()
 #include <math.h>     // pow()
+#include <iostream>   // std
 #include <string>     // string
 #include <sys/wait.h> // wait()
-#include <unistd.h>   // _exit(), fork(), sysconf(), EXIT_FAILURE, EXIT_SUCCESS
-
-/**
- * @brief Mostra mostra o ID do pai e do processo em execução
- *
- * @param level Nível atual do processo
- */
-void pstree(int level) {
-  std::string out = "(Nível:" + std::to_string(level) + ")";
-  for (int i = 0; i < level - 1; i++) {
-    out += "    ";
-  }
-  out += "PPID[" + std::to_string(getppid()) + "]:";
-  out += "PID[" + std::to_string(getpid()) + "]";
-  std::printf("%s\n", out.c_str());
-}
+#include <unistd.h>   // exit() fork(), sysconf(), execlp()
+#include <chrono>     // chrono
+#include <thread>     // this_thread
 
 /**
  * @brief Cria uma árvore de processos
  *
- * Cada processo cria um nó filho a esquerda e a direita, até o nível máximo ser
+ * Cada processo cria dois filhos, até o nível máximo ser
  * atingido.
  *
  * @param level Nível atual do processo
  * @param max_level Nível máximo que os processos podem chegar
  */
-void process_tree(int level, const int max_level) {
-  if (level > max_level) {
-    return;
+void create_processes(int level, int max_level) {
+  if(level >= max_level) return;
+    
+  // Cria o primeiro filho
+  pid_t first_child = fork();
+  
+  // O primeiro filho chama novamente a função create_processes
+  if (first_child == 0) {
+    create_processes(level+1, max_level);
+    // Aguarda interação com input do usuário para finalizar processo
+    std::cin.get();            
+    exit(0);
   }
 
-  pstree(level);
-
-  // Cria o processo filho a esquerda
-  pid_t left_child = fork();
-
-  // Se menor que zero houve falha na criação
-  if (left_child < 0) {
-    printf("Process(%d): fail %d\n", getpid(), left_child);
-    _exit(EXIT_FAILURE);
+  // Cria o segundo filho
+  pid_t second_child = fork();
+  
+  // O segundo filho chama novamente a função create_processes
+  if (second_child == 0) {
+    create_processes(level+1, max_level);
+    // Aguarda interação com input do usuário para finalizar processo
+    std::cin.get();
+    exit(0); 
   }
-
-  // Se a criação resultou em zero, este é o processo filho
-  if (left_child == 0) {
-    // Chama recusivamente a função para criar seus processos filhos
-    process_tree(level + 1, max_level);
-    _exit(EXIT_SUCCESS);
-  }
-
-  // Cria o processo filho a direita
-  pid_t right_child = fork();
-
-  // Se menor que zero houve falha na criação
-  if (right_child < 0) {
-    printf("Process(%d): fail %d\n", getpid(), right_child);
-    _exit(EXIT_FAILURE);
-  }
-
-  // Se a criação resultou em zero, este é o processo filho
-  if (right_child == 0) {
-    process_tree(level + 1, max_level);
-    _exit(EXIT_SUCCESS);
-  }
-
-  // Espera pelo processo a direita e a esquerda terminarem
-  wait(NULL);
-  wait(NULL);
 }
 
 int main(int argc, char **argv) {
+  // Caso não tenho os argumentos necessarios imprime exemplo
   if (argc < 2) {
-    printf("%s <num_process>\n", argv[0]);
+    std::cout << argv[0] << " <num_process>" << std::endl;
     return 1;
   }
-
+  
+  // Nível de camadas
   const int N = atoi(argv[1]);
-
+  
   // Verifica se é um número válido de processos no sistema.
   // Para isso ele deve ser maior que 1 e menor que o limite de filhos que um
   // processo pode ter.
   if (N < 1 || sysconf(_SC_CHILD_MAX) < static_cast<long int>(pow(2, N))) {
-    printf("%d é um número inválido de processos", N);
+    std::cout << N <<" é um número inválido de processos" << std::endl;
     return 1;
   }
+  
+  // Captura o PID do processo atual
+  pid_t pid = getpid();
+  
+  // Inicia a produção dos processos filho
+  create_processes(0, N);
 
-  process_tree(1, N);
-
-  // Espera os processos filhos terminarem
-  while (wait(NULL) >= 0) {
+  // Atualiza a visualização da hierarquia de processos
+  while (true) {
+    // Cria processo para executar comando clear
+    pid_t pid_clear = fork();
+    if(pid_clear == 0){
+      // Limpa a tela
+      execlp("clear", nullptr);
+      exit(0);
+    }
+    // Aguarda o processo filho de limpar a tela
+    waitpid(pid_clear, nullptr, 0);
+    
+    std::cout << "Hierarquia de processos para o PID " << pid << ":" << std::endl;
+    
+    // Executa o pstree para o processo main
+    std::string command_pstree = "pstree -p " + std::to_string(pid); 
+    system(command_pstree.c_str());
+    
+    // Espera um pouco antes de atualizar novamente
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    
+    // Verifica se todos os filhos terminaram
+    if (waitpid(-1, nullptr, WNOHANG) > 0) {
+        break; // Se algum filho terminou, sai do loop
+    }
   }
 
   return 0;
