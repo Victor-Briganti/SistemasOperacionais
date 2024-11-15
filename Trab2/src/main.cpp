3c3a03e6290d6b28994a30aaafbde90290b66a2a
Victor Briganti <jvbriganti@gmail.com>
2024-11-15 10:26:18 -0300

    Trab2: Estrutura inicial

---
+++ Trab2/src/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * Descrição: Arquivo main para o FAT32 shell
+ *
+ * Autores: Hendrick Felipe Scheifer, João Victor Briganti, Luiz Takeda
+ * Licença: BSD 2
+ *
+ * Data: 15/11/2024
+ */
+
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+  if (argc < 2) {
+    std::cout << "Número incorreto de argumentos.\n";
+    std::cout << "Exemplo: ";
+    std::cout << argv[0] << " fat32.img\n";
+    return -1;
+  }
+
+  std::fstream image(argv[1], std::ios::in | std::ios::out | std::ios::binary);
+
+  if (!image.is_open()) {
+    std::cout << "Falha ao ler o arquivo: '" << argv[1] << "'.\n";
+    return -1;
+  }
+}
