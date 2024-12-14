b62c37b90cae18ce16474fde64cbd5d8c7582f93
Victor Briganti <jvbriganti@gmail.com>
2024-12-14 14:26:26 -0300

    Trab1: Alterado estrutura do audience

--- Trab1/audience.cpp
+++ Trab1/audience.cpp
@@ -17,19 +17,22 @@ void assiste_testes(int idAudience) {
   sleep(watchTime);
 }
 
-void *entra_salao(void *arg) {
+void entra_salao(Audience *audience) {
+  // Aumenta o número de pessoas na audiência querendo ver a sessão
+  pthread_mutex_lock(audience->mutexWaitingAudience);
+  std::printf("[Audience %d] Esperando na fila\n", audience->id);
+  (*audience->waitingAudience)++;
+  pthread_mutex_unlock(audience->mutexWaitingAudience);
+
+  sem_wait(audience->semWaitingAudience);
+}
+
+void *start(void *arg) {
   Audience *audience = static_cast<Audience *>(arg);
 
   // Assiste as sessões enquanto elas existirem
   while (*(audience->sessionOver) == false) {
-    // Aumenta o número de pessoas na audiência querendo ver a sessão
-    pthread_mutex_lock(audience->mutexWaitingAudience);
-    (*audience->waitingAudience)++;
-    pthread_mutex_unlock(audience->mutexWaitingAudience);
-
-    std::printf("[Audience %d] Esperando na fila\n", audience->id);
-    sem_wait(audience->semWaitingAudience);
-
+    entra_salao(audience);
     assiste_testes(audience->id);
     sai_salao(audience->id);
   }
@@ -41,7 +44,7 @@ void *entra_salao(void *arg) {
 
 int init_audience(std::vector<pthread_t> tid, std::vector<Audience> audience) {
   for (size_t i = 0; i < tid.size(); i++) {
-    if (pthread_create(&tid[i], nullptr, entra_salao, &audience[i]) != 0) {
+    if (pthread_create(&tid[i], nullptr, start, &audience[i]) != 0) {
       std::printf("[Audience %zu] ", i);
       std::perror("pthread_create ");
     }
