2edbead11f6c53145cd6b08e68b576f53de0dd69
Victor Briganti <jvbriganti@gmail.com>
2025-01-01 16:29:02 -0300

    Trab2 [refactor]: Alterado funções de data/hora

--- Trab2/src/filesystem/dir.cpp
+++ Trab2/src/filesystem/dir.cpp
@@ -22,3 +22,25 @@ BYTE shortCheckSum(const char *shortName)
 
   return Sum;
 }
+
+WORD day(WORD date) { return date & 0x1F; }
+
+WORD month(WORD date) { return (date >> 5) & 0x0F; }
+
+WORD year(WORD date) { return ((date >> 9) & 0xFF) + 1980; }
+
+DWORD dateStamp(DWORD day, DWORD month, DWORD year)
+{
+  return day | (month << 5) | ((year - 1980) << 9);
+}
+
+WORD hour(WORD time) { return (time >> 11) & 0xFF; }
+
+WORD minutes(WORD time) { return (time >> 5) & 0x3F; }
+
+WORD seconds(WORD time) { return ((time >> 9) & 0xFF) + 1980; }
+
+DWORD timeStamp(DWORD hour, DWORD minutes, DWORD seconds)
+{
+  return (hour << 11) | (minutes << 5) | (seconds / 2);
+}
